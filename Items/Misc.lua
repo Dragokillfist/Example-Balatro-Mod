ee2feaed365f6f1665367e2cb79df8abdc575cf0
dragokillfist <tinker198431@gmail.com>
2025-05-01 17:51:16 -0500

    adds a rough explenation of the pokerhand and more indepth of what you will find in the lua files inside of the main.lua

--- Items/Misc.lua
+++ Items/Misc.lua
@@ -4,25 +4,25 @@ SMODS.PokerHand {
     mult = 9,
     l_chips = 40,
     l_mult = 4,
-    example = {
-        { 'S_A',    true },
-        { 'S_K',    true },
-        { 'S_Q',    true },
-        { 'S_J',    true },
-        { 'S_T',    true },
+    example = {-- this is the example that is shown when hovering over the hand in the run info tab. these cards are all the spades suit
+        { 'S_A',    true }, -- Ace
+        { 'S_K',    true }, -- King
+        { 'S_Q',    true }, -- Queen
+        { 'S_J',    true }, -- Jack
+        { 'S_T',    true }, -- 10
     },
     loc_vars = function(self, info_queue, card)
-        info_queue[#info_queue + 1] = { key = "exg_hand_Royal_Flush", set = "misc" }
+        info_queue[#info_queue + 1] = { key = "exg_hand_Royal_Flush", set = "misc" } -- this is how we set the text that is shown when overing over the hand type in the run info tab
     end,
     evaluate = function(parts, hand)
-        if next(parts._flush) and next(parts._straight) then
-            local _strush = SMODS.merge_lists(parts._flush, parts._straight)
-            local royal = true
-            for j = 1, #_strush do
-                local rank = SMODS.Ranks[_strush[j].base.value]
-                royal = royal and (rank.key == 'Ace' or rank.key == '10' or rank.face)
+        if next(parts._flush) and next(parts._straight) then -- chaecks if the selected cards make up both a flush and a straight
+            local _strush = SMODS.merge_lists(parts._flush, parts._straight) -- this is a local value we make to make it easier to code in, as well as making everything look more neat
+            local royal = true -- this is a local boolean value that we use in the for loop to check if the cards are a royal flush or not
+            for j = 1, #_strush do -- this for loop checks the number of cards that satisfy "_strush" and checks if they are a royal flush or not
+                local rank = SMODS.Ranks[_strush[j].base.value] -- this is the rank 
+                royal = royal and (rank.key == 'Ace' or rank.key == '10' or rank.face) -- this is where we define our local boolean value, and we check if the rank is a royal flush or not
             end
-            if royal then return {_strush} end
+            if royal then return {_strush} end -- if the cards are a royal flush then we return the cards that are a royal flush, and this is what will be used to calculate the chips and mult of the hand
         end
     end,
 }
--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ ExampleMod = SMODS.current_mod
 ExampleMod_config = ExampleMod.config -- this is the config file that is used to store the mods settings, and it is used to make sure that the mod is loaded correctly
 ExampleMod.enabled = copy_table(ExampleMod_config) -- this is the current state of the mods settings, and it is used to make sure that the mod is loaded correctly
 
--- the three things above are required to make your mods config actually work
+-- to make your config actually register and work you will need to add the lines 3, 4, and 5 to your mod, and you can replace the ExampleMod with your mods name
 
 -- When making a Sprite For balatro you need to have a 1x and a 2x file, because the sprite in the collection and the sprite of the joker you have mid run are diffrent resolutions/sizes
 SMODS.Atlas({
@@ -18,6 +18,6 @@ SMODS.Atlas({
 -- this is where we will regester other files from within this mods folder such as stuff from our Items folder, tho if you dont want to load that file you can comment it out by adding "--" aty the start of the line
 -- when setting the files path you need to make sure that you are using the correct capitalization, because if you dont, your mod will crash on linux platforms
 assert(SMODS.load_file("Items/Jokers.lua"))()
-assert(SMODS.load_file("Items/Consumables.lua"))()
+assert(SMODS.load_file("Items/Consumables.lua"))() -- this is where you will find how you can make a custom consumable type or just create a consumable of a pre-existing type
 assert(SMODS.load_file("Items/Misc.lua"))()
-assert(SMODS.load_file("Lib/Utility.lua"))()
+assert(SMODS.load_file("Lib/Utility.lua"))() -- this is the file where we add the code to initialize the config menu and other utility functions
