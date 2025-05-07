f07f2a48c59250a2302f08c5458ccdd7cb796b32
dragokillfist <tinker198431@gmail.com>
2025-05-07 14:29:01 -0500

    adds "Lucky 7's" joker and a comment on what it does (Code mostly made by playerWon from the Cryptid discord)

--- Items/Jokers.lua
+++ Items/Jokers.lua
@@ -127,3 +127,32 @@ SMODS.Joker {
         end
     end,
 }
+
+-- code provided from playerWon from the Cryptid discord
+SMODS.Joker {
+    key = "Lucky7's",
+    pos = {x = 0, y = 0},
+    rarity = 3,
+    atlas = "PLH",
+    config = { extra = { X_mult = 1, mult_mod = 0.1 } },
+    cost = 6,
+    loc_txt = {
+        name = "Lucky 7's",
+        text = {
+            "insert text here"
+        }
+    },
+    loc_vars = function (self, info_queue, center)
+        return { vars = { center.ability.extra.X_mult, center.ability.extra.mult_mod } }
+    end,
+    calculate = function (self, card, context)
+        if context.individual and context.other_card.lucky_trigger then -- lucky_trigger is a context that checks if a lucky cards ability was triggered
+            card.ability.extra.X_mult = card.ability.extra.X_mult + card.ability.extra.mult_mod 
+        end
+        if context.individual and context.cardarea == G.play then
+            if context.other_card.base.nominal == 7 then
+                return {xmult = card.ability.extra.X_mult}
+            end
+        end
+    end,
+}
