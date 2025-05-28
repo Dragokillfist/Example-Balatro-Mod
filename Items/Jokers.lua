439d94b71eab4cc60343644ab7355a3984498d0f
dragokillfist <tinker198431@gmail.com>
2025-05-28 11:02:21 -0500

    switch from center to card in the loc_vars

--- Items/Jokers.lua
+++ Items/Jokers.lua
@@ -7,8 +7,8 @@ SMODS.Joker{ -- this is a joker that uses a localization file, you can modify th
     atlas = "PLH", -- this is the key that determines what atlas the joker will use, this is the same as the key in the atlas function in main.lua
     config = { extra = {mult = 4} },  -- the config is used to make basic effects for our jokers
     cost = 6, -- this is the cost of the joker in the shop, and its sell value is half of what the cost to buy is, so this joker would sell for 3 dollars
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.mult } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
     end,
     calculate = function (self, card, context)
         if context.cardarea == G.jokers and context.joker_main then -- the context that is used to make the jokers ability trigger after cards have finished scoring
@@ -34,8 +34,8 @@ SMODS.Joker{
             "{C:inactive} this joker uses loc_txt instead of the en-us.lua file"-- the {C:inactive} is what gives the text the transparent gray color and #1# is the first variable that is in the jokers config and the {} ends the colored text without having to start a new line
         }
     },
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.mult } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
     end,
     calculate = function (self, card, context)
         if context.cardarea == G.jokers and context.joker_main then
@@ -53,8 +53,8 @@ SMODS.Joker{
     atlas = "PLH",
     config = { extra = {mult = 4} },
     cost = 6,
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.mult } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
     end,
     calculate = function (self, card, context)
         if context.cardarea == G.jokers and context.joker_main then
@@ -71,8 +71,8 @@ SMODS.Joker {
     atlas = "PLH",
     config = { extra = {X_mult = 2, mult_mod = 0.5} },
     cost = 6,
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.X_mult, center.ability.extra.mult_mod } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.X_mult, card.ability.extra.mult_mod } }
     end,
     calculate = function (self, card, context)
         if context.individual and context.cardarea == G.play then 
@@ -91,8 +91,8 @@ SMODS.Joker {
     atlas = "PLH",
     config = { extra = {mult = 4} },
     cost = 6,
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.mult } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mult } }
     end,
     calculate = function (self, card, context)
         if context.cardarea == G.play and context.individual then
@@ -110,8 +110,8 @@ SMODS.Joker {
     atlas = "PLH",
     config = { extra = {mult = 4, chips = 10} },
     cost = 6,
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.mult, center.ability.extra.chips } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mult, card.ability.extra.chips } }
     end,
     calculate = function (self, card, context)
         if context.cardarea == G.play and context.individual then
@@ -142,8 +142,8 @@ SMODS.Joker {
             "insert text here"
         }
     },
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.X_mult, center.ability.extra.mult_mod } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.X_mult, card.ability.extra.mult_mod } }
     end,
     calculate = function (self, card, context)
         if context.individual and context.other_card.lucky_trigger then -- lucky_trigger is a context that checks if a lucky cards ability was triggered
@@ -164,8 +164,8 @@ SMODS.Joker {
     atlas = "PLH",
     config = { extra = {mult = 4, storage_req = 3, mult_mod = 2, intensity = 0, storage = 0 }, exmp_storage = true}, -- we declare exmp_storage to be true so then the joker has the use storage button, the function we use to make it is in Lub/Utility.lua and we patch the button into the UI_definitions.lua in the lovely dump under dump/functions/UI_definitions.lua, tho the code for it can be found in out example.toml in our lovely folder
     cost = 6,
-    loc_vars = function (self, info_queue, center)
-        return { vars = { center.ability.extra.mult, center.ability.extra.storage_req, center.ability.extra.mult_mod, center.ability.extra.intensity, center.ability.extra.storage } }
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.mult, card.ability.extra.storage_req, card.ability.extra.mult_mod, card.ability.extra.intensity, card.ability.extra.storage } }
     end,
     calculate = function (self, card, context)
         card.ability.extra.storage = 0 + G.GAME.storage -- this is to display our current storage value on the joker in our localization file, this value is defined as #5#
